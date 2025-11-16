06a59a69451b55d46a11c5f54c14d93f7f6f96c5
BernatDeveloper <bernifogi@gmail.com>
2025-11-16 19:25:33 +0100

    Add: project folder structure

---
+++ src/data/projects.ts
@@ -0,0 +1,28 @@
+import type { Project } from "../types/project";
+
+export const projects: Project[] = [
+  {
+    title: "Events App",
+    description: "EventsApp es una plataforma web para la gestión completa de eventos, permitiendo invitar a otros usuarios y organizar todos los detalles desde un solo lugar. El proyecto está construido con React, Laravel, React Toolkit, Tailwind CSS y Leaflet para la integración de mapas.",
+    img: "/images/projects/EventsApp.jpg",
+    url: "https://eventos-app.vercel.app/"
+  },
+  {
+    title: "React components",
+    description: "Explora una colección de componentes personalizables para React. Cada componente incluye ejemplos interactivos, código JSX y estilos CSS, diseñados para facilitar el desarrollo de tus aplicaciones web.",
+    img: "/images/projects/ReactComponentsImg.avif",
+    url: "https://components-react-storage.vercel.app/"
+  },
+  {
+    title: "Mis Notas",
+    description: "Esta es una aplicación web donde los usuarios pueden registrarse, crear notas, gestionarlas y organizarlas según su estado. Además, cuenta con un perfil donde los usuarios pueden manejar diferentes ajustes personalizados.",
+    img: "/images/projects/MisNotasImg.avif",
+    url: "https://github.com/BernatDev6/Todo_React_Laravel"
+  },
+  {
+    title: "Coming soon...",
+    description: "This is the third project, featuring a responsive layout.",
+    img: "https://picsum.photos/id/36/300",
+    url: "#"
+  }
+];
